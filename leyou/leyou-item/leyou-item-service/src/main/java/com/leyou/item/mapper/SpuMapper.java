a4511d521af5cf77831ef060a14189762d20f777
wxinxings <wxinxings@icloud.com>
2020-06-29 16:56:42 +0800

    第十五天代码
    在安装RabbitMQ时不知道动了什么文件，Linux系统出问题了，之后一晚上都在重新装Linux服务器，功夫不负有心人，在凌晨一点多弄完。之后练习RabbitMQ教程发现了Controller里存在路径和前端不对应的问题，进行了修改，工程已经正常运行。

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/SpuMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Spu;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author HackerStar
+ * @create 2020-06-15 16:01
+ */
+public interface SpuMapper extends Mapper<Spu> {
+}
