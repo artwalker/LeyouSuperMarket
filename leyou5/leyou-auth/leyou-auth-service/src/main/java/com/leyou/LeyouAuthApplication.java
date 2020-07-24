4e8d9fd2dba23b1b930682249547fbb698075f0e
wxinxings <wxinxings@icloud.com>
2020-07-24 16:12:31 +0800

    提交

---
+++ leyou5/leyou-auth/leyou-auth-service/src/main/java/com/leyou/LeyouAuthApplication.java
@@ -0,0 +1,19 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author HackerStar
+ * @create 2020-07-03 09:21
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class LeyouAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouAuthApplication.class);
+    }
+}
---
+++ leyou5/leyou-user/leyou-user-service/src/main/java/com/leyou/user/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.user.mapper;
+
+import com.leyou.user.pojo.User;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author HackerStar
+ * @create 2020-07-01 15:05
+ */
+public interface UserMapper extends Mapper<User> {
+}
